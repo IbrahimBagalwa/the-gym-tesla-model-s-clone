edaeb7a2b1132a7676e507133e81ae1f71d9cb9f
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2022-09-26 14:43:41 +0200

    update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: 'Poppins, sans-serif',
+      },
+      colors: {
+        primary: '#FF5A5F',
+      },
+    },
+  },
+  plugins: [],
+};
